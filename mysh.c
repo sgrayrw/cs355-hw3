42bf1897c42f1aa78767abdf7effbcc0bad955c3
Yuxiao Wang <me@rayrw.com>
2019-10-01 17:23:03 -0400

    update framework

--- mysh.c
+++ mysh.c
@@ -1,5 +1,5 @@
 // builtin functions
-typedef builtin_func ... // type for builtin functions
+typedef builtin_func void (builtin_func)(void) // type for builtin functions
 builtin_func get_builtin(); // return the appropriate built-in func based on `tokens`, NULL if no match
 void my_exit();
 void jobs(); // list all background jobs
@@ -9,16 +9,16 @@ void bg(); // continue a specific job in the background
 
 // job related
 struct Node {
-  struct Job job;
-  Node next;
+    struct Job job;
+    Node next;
 };
 
 struct Job {
-  int jid; // job id
-  pid_t pid;
-  enum Status {Running, Suspended};
-  char* args; // args that started the job
-  // terminal modes
+    int jid; // job id
+    pid_t pid;
+    enum Status {Running, Suspended};
+    char* args; // args that started the job
+    // terminal modes
 };
 
 Node jobs; // head of linked list
@@ -45,41 +45,40 @@ bool is_background(); // check if line ends with `&` (background job), AND remov
 // *********************************** data structure/function defs and global vars above ***********************************
 
 int main() {
-  initialize_handlers(); // register for signal handlers
-  
-  while (1) {
-    read_line(); // read into line buffer
-    parse_line(); // parse arguments
-    eval(); // evaluate arguments
-    free_storage(); // free spaces
-  }
-  return 0;
+    initialize_handlers(); // register for signal handlers
+
+    while (1) {
+        read_line(); // read into line buffer
+        parse_line(); // parse arguments
+        eval(); // evaluate arguments
+        free_storage(); // free spaces
+    }
+    return 0;
 }
 
 void eval(){
-  builtin_func func = get_builtin();
-  if (func) {
-    func();
-    return;
-  }
-  
-  // exec call
-  pid_t pid = fork();
-  if (pid == 0) { // child
-    // need to set signal handlers back (to default behavior)
-    setpgrp();
-    exec();
-  } else { // parent
-    if (is_background()) {
-      sigprocmask(BLOCK, &sigset, NULL); // mask SIGCHLD to protect CS
-      add_job(pid);
-      sigprocmask(UNBLOCK, &sigset, NULL);
-      return;
-    } else {
-      tcsetpgrp(0, pid);
-      waitpid(pid, &status);
+    builtin_func func = get_builtin();
+    if (func) {
+        func();
+        return;
     }
-  }
-  
-}
 
+    // exec call
+    pid_t pid = fork();
+    if (pid == 0) { // child
+        // need to set signal handlers back (to default behavior)
+        setpgrp();
+        exec();
+    } else { // parent
+        if (is_background()) {
+            sigprocmask(BLOCK, &sigset, NULL); // mask SIGCHLD to protect CS
+            add_job(pid);
+            sigprocmask(UNBLOCK, &sigset, NULL);
+            return;
+        } else {
+            tcsetpgrp(0, pid);
+            waitpid(pid, &status);
+        }
+    }
+
+}
